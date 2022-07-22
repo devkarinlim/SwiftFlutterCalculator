b31605efff49c136979d53fa3dfb5700679f7e35
BCA-GSIT-iMac-6 <monna_asnawi@bca.co.id>
2022-07-22 10:06:29 +0700

    karin initial commit

--- SwiftFlutterCalculator/ViewController.swift
+++ SwiftFlutterCalculator/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var flutterButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func openFlutterPage(_ sender: Any) {
+    }
+    
 }
 
