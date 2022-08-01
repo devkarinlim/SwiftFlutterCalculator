663a463ae609fc51bb0101874d53b64bd1376de8
BCA-GSIT-iMac-6 <monna_asnawi@bca.co.id>
2022-08-01 13:18:24 +0700

    karin done flutter pod

--- SwiftFlutterCalculator/ViewController.swift
+++ SwiftFlutterCalculator/ViewController.swift
@@ -6,16 +6,21 @@
 //
 
 import UIKit
+import Flutter
 
 class ViewController: UIViewController {
     @IBOutlet var flutterButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.isHidden = true
         // Do any additional setup after loading the view.
     }
 
     @IBAction func openFlutterPage(_ sender: Any) {
+        let flutterEngine = (UIApplication.shared.delegate as! AppDelegate).flutterEngine
+        let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
+        navigationController?.pushViewController(flutterViewController, animated: true)
     }
     
 }
--- SwiftFlutterCalculatorUITests/SwiftFlutterCalculatorUITests.swift
+++ SwiftFlutterCalculatorUITests/SwiftFlutterCalculatorUITests.swift
@@ -26,6 +26,10 @@ class SwiftFlutterCalculatorUITests: XCTestCase {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
+        measure {
+            
+            
+        }
 
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
